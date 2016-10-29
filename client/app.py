d80525212bec6e3d3dfa740d78c1c7f1285dcbc8
shun3475 <imokenpi3475@gmail.com>
2016-10-29 23:18:20 +0900

    Move client/accelaration.py

--- client/app.py
+++ client/app.py
@@ -12,6 +12,7 @@ import base64
 import json
 import uuid
 from lib.camera import Camera
+from lib.acceleration import Acceralation
 from lib import jtalk
 from lib import ir_sensor
 
@@ -24,22 +25,24 @@ def on_message(ws, message):
     distance = 5
     jtalk.speak(text)
     jtalk.speak("障害物までの距離は%sメートルです" % distance)
-    ws.close()
 
 def on_error(ws, error):
     print error
+    ws.close()
 
 def on_close(ws):
     print "### closed ###"
 
 def on_open(ws):
     camera = Camera()
-    filepath = uuid.uuid4()
-    filename = '%s.jpg' % filepath
-    camera.snapshot(filename)
-    file = open(filename, "rb").read()
-    file_data = base64.b64encode(file)
-    ws.send(json.dumps({'upload_file': file_data})) 
+    acceleration = Acceralation()
+    while True:
+        filepath = uuid.uuid4()
+        filename = '%s.jpg' % filepath
+        camera.snapshot(filename)
+        file = open(filename, "rb").read()
+        file_data = base64.b64encode(file)
+        ws.send(json.dumps({'upload_file': file_data})) 
 
 if __name__ == "__main__":
     ws_url = "ws://localhost:3000"
